2759aec4385519380a85671687255c01e47f5c33
Annatroken <117992590+Annatroken@users.noreply.github.com>
2026-07-09 09:21:05 +0200

    __init__.py erstellen

---
+++ BattleOfTheBooks/lehrer/__init__.py
@@ -0,0 +1,6 @@
+from .mathelehrer import Mathelehrer
+from .deutschlehrer import Deutschlehrer
+from .sportlehrer import Sportlehrer
+from .chemielehrer import Chemielehrer
+from .erdkundelehrer import Erdkundelehrer
+from .schulleiter import Schulleiter
