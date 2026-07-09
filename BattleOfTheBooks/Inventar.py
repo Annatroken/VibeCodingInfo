ba027965e868dd301d8128e5344afa49c88493d5
Annatroken <117992590+Annatroken@users.noreply.github.com>
2026-07-09 09:22:27 +0200

    Inventar.py aktualisieren

--- BattleOfTheBooks/Inventar.py
+++ BattleOfTheBooks/Inventar.py
@@ -8,7 +8,29 @@ class Inventar:
 
         self.buecher.append(buch)
 
+        print(f"{buch.titel} wurde hinzugefügt.")
+
+    def buch_entfernen(self, buch):
+
+        if buch in self.buecher:
+            self.buecher.remove(buch)
+
     def anzeigen(self):
 
+        print("Inventar:")
+
+        if len(self.buecher) == 0:
+            print("Keine Bücher vorhanden.")
+
         for buch in self.buecher:
-            print(buch.titel)
+            print("-", buch.titel)
+
+    def buch_verwenden(self, nummer, lehrer):
+
+        if nummer < len(self.buecher):
+
+            buch = self.buecher[nummer]
+
+            buch.verwenden(lehrer)
+
+            self.buecher.remove(buch)
