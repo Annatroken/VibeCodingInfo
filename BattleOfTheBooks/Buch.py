9e279ac644c61fc21e721d12489b681cca747b2b
Annatroken <117992590+Annatroken@users.noreply.github.com>
2026-07-09 09:21:53 +0200

    Buch.py aktualisieren

--- BattleOfTheBooks/Buch.py
+++ BattleOfTheBooks/Buch.py
@@ -1,10 +1,16 @@
 class Buch:
 
-    def __init__(self, titel, bonus):
+    def __init__(self, titel, angriff=0, verteidigung=0, energie=0):
 
         self.titel = titel
-        self.bonus = bonus
+        self.angriff = angriff
+        self.verteidigung = verteidigung
+        self.energie = energie
 
     def verwenden(self, lehrer):
 
-        lehrer.angriff += self.bonus
+        lehrer.angriff += self.angriff
+        lehrer.verteidigung += self.verteidigung
+        lehrer.energie += self.energie
+
+        print(f"{lehrer.name} benutzt '{self.titel}'.")
